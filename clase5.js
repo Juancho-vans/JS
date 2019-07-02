a922e8874babdc5196a1c89fd0b23ae56ce35388
Juan Carlos Castillo  PC <castillolopezjuan08@gmail.com>
2019-07-02 13:33:10 -0500

    ultimos cambios del ejercicio

--- clase5.js
+++ clase5.js
@@ -1,28 +1,42 @@
 var nombre = "Juan Carlos";
 var edad = 37;
 var nombreMayus;
-
-
-
+//creando el objeto JCCL
+var JCCL = {
+    nombre:"Juan Carlos",
+    apellidoP:"Castillo",
+    apellidoM:"Lopez",
+    edad:37
+    //llave:valor
+}
+var SITC = {
+    nombre:'Saul Ivan',
+    apellidoP:"Torres",
+    apellidoM:"Chavez",
+    edad:37
+    //llave:valor ó Key:value
+}
 function imprimirEdad(nom, ed) {
     console.log( `${nom} tiene ${ed} años`)
 }
 // la comilla invertida ` se usa para colocar 
 // codigo dentro de ella
-
-imprimirEdad()
 imprimirEdad("Juan Carlos",37)
 imprimirEdad("Natalia",6)
 
 /*
-el codigo ascci para la comilla invertida es alt + 96
+el codigo ascci para la comilla invertida es 
+alt + 96
 ``
 */
 
-function nombreMayusculas (nombreMayus) {
-    console.log(nombreMayus.toUpperCase())
-}
+nombreMayusculas(JCCL)
+nombreMayusculas(SITC)
+nombreMayusculas({nombre:"Natalia",
+            apellidoP:"castillo",
+            apellidoM:"lopez", 
+            edad:6})
 
-nombreMayusculas("juan carlos")
-nombreMayusculas("natalia")
-nombreMayusculas("anacleta")
+function nombreMayusculas ({nombre}) {
+    console.log(nombre)
+}

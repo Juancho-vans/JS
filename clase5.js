944643631dd756ce2680ec2d2c00fe83343f07c6
Juan Carlos Castillo  PC <castillolopezjuan08@gmail.com>
2019-07-02 13:10:24 -0500

    se agrego una fuincion mas a alcancefunciones

--- clase5.js
+++ clase5.js
@@ -1,12 +1,14 @@
-var nombre = "juan Carlos"
+var nombre = "Juan Carlos";
 var edad = 37;
+var nombreMayus;
 
 
 
 function imprimirEdad(nom, ed) {
     console.log( `${nom} tiene ${ed} años`)
 }
-// la comilla invertida ` se usa para colocar codigo dentro de ella
+// la comilla invertida ` se usa para colocar 
+// codigo dentro de ella
 
 imprimirEdad()
 imprimirEdad("Juan Carlos",37)
@@ -16,3 +18,11 @@ imprimirEdad("Natalia",6)
 el codigo ascci para la comilla invertida es alt + 96
 ``
 */
+
+function nombreMayusculas (nombreMayus) {
+    console.log(nombreMayus.toUpperCase())
+}
+
+nombreMayusculas("juan carlos")
+nombreMayusculas("natalia")
+nombreMayusculas("anacleta")
